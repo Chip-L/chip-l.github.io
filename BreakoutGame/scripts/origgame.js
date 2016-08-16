72ac502196d0fedd9abebe742ef2dbeaf7d622d2
Chip Long <chip_long@yahoo.com>
2016-08-15 21:08:46 -0600

    Adding Breakout

---
+++ BreakoutGame/scripts/origgame.js
@@ -0,0 +1,200 @@
+var SHOW_TEST_DATA = true;
+
+var myGamePiece;
+var myGameBall;
+var myObstacles = [];
+var myScore;
+var myScoreValue = 0;
+var WIDTH = 480;
+var HEIGHT = 270;
+
+$(function startGame() {
+    myGameArea.start();
+	myScore = new component("30px", "Consolas", "black", 280, 40, 0, "text");
+    myGamePiece = new component(140, 10, "red", 170, 250, 0, "brick");
+	
+	myGameBall = new component(10, 10, "blue", 150, 235, 0, "ball");
+	myGameBall.speedX = 1;	// + = right; - = left
+	myGameBall.speedY = -1;	// + = down; - = up
+	
+	myObstacles.push(myGamePiece);
+	myObstacles.push(new component(WIDTH, 1, "black", 0, 0, 100, "brick"));
+	myObstacles.push(new component(1, HEIGHT, "black", 0, 0, 50, "brick"));
+	myObstacles.push(new component(1, HEIGHT, "black", WIDTH, 0, 25, "brick"));
+	
+	$("#end_game").fadeOut(500);
+	
+	if (SHOW_TEST_DATA) {
+		showTestData_GamePiece = new component("15px", "Ariel", "black", 360, 180, 0, "text");		
+		showTestData_GameBall = new component("15px", "Ariel", "black", 360, 200, 0, "text");
+	}
+})
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = WIDTH;
+        this.canvas.height = HEIGHT;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+		this.interval = setInterval(updateGameArea, 20); // 20ms = 50fps
+			
+		// set up event listeners ON THE WINDOW (not an object)
+		window.addEventListener('mousemove', function (e) { // follows mouse movement
+			myGameArea.x = e.pageX; // only horizontal, get x only
+		})
+		window.addEventListener('touchmove', function (e) { // follows touch movement
+			myGameArea.x = e.pageX; // only horizontal, get x only
+		})
+		window.addEventListener('keydown', function (e) { // keyboard moves key down moves, key up stops movement
+			myGameArea.key = e.keyCode;
+		})
+		window.addEventListener('keyup', function (e) {
+			myGameArea.key = false;
+		})
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+function everyInterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+/*
+component needs to extend to brick and ball. Bricks are square and are obstacles that the ball crashes in to - this is both the brick and the paddle. The ball should also be round or else a graphic. Ball extension can have the crashWith function, the others don't need it. Everything will need the height, width, and speed variables - it is tempting to move speed to the ball only, but I think this would be a mistake because of the paddle - both could be extended, but then it is still duplication of code... The ball will have to bounce off the obstacles, walls and ceiling, but not the floor. the ball should bounce at varying speeds(?) and angles (look up the physics/math - will the ball and/or the obstacles need elasticity?)
+
+I'm not sure if frameNo and everyInterval are needed.
+*/
+function component(width, height, color, x, y, scoreValue, type) {
+	this.type = type;
+	this.scoreValue = scoreValue; // this is returned when a brick is hit and has a value (walls and paddle will be 0)
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+	this.update = function() {
+		ctx = myGameArea.context;
+		ctx.fillStyle = color;
+		if (this.type === "text") {
+			ctx.font = this.width + " " + this.height;
+			ctx.fillText(this.text, this.x, this.y);
+		} else if (this.type === "brick") {
+			ctx.fillRect(this.x, this.y, this.width, this.height);
+		} else {
+			var radius = this.width/2; // this assumes perfect circle coordinates were entered
+			ctx.beginPath();
+			ctx.arc(this.x + radius, this.y + radius, radius, 0, 2 * Math.PI, false);
+			ctx.closePath();
+			ctx.fill();
+		}
+	};
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY; 
+    };
+    this.crashWith = function(otherobj) {
+		// this objects sides
+        var myLeft = this.x;
+        var myRight = this.x + (this.width);
+        var myTop = this.y;
+        var myBottom = this.y + (this.height);
+		
+		// other object's sides
+        var otherLeft = otherobj.x;
+        var otherRight = otherobj.x + (otherobj.width);
+        var otherTop = otherobj.y;
+        var otherBottom = otherobj.y + (otherobj.height);
+		
+		// determine crash return the side hit
+        var crash = "miss";
+		if ((myBottom < otherTop) ||
+            (myTop > otherBottom) ||
+            (myRight < otherLeft) ||
+            (myLeft > otherRight)) {
+           crash = "miss";
+        } else if (myBottom >= otherTop) {
+			crash = "bottom";
+		} else if (myTop <= otherBottom) {
+			crash = "top";
+		} else if (myRight >= otherLeft) {
+			crash = "right";
+		} else if (myLeft <= otherRight) {
+           crash = "left";
+        }
+        return crash;
+    };
+}
+
+function updateGameArea() {
+	var x, y, sideHit;
+	
+	// out of bounds
+	if ((myGameBall.y + myGameBall.height) > myGameArea.canvas.height) {
+		myGameArea.stop();
+		$("#end_game").fadeIn(500);
+		return;
+	}
+	
+	myGameArea.clear();
+	myGameArea.frameNo++;
+	
+	// set ball movement
+	for (var i = 0; i < myObstacles.length; i++) {
+		sideHit = myGameBall.crashWith(myObstacles[i]);
+		if (sideHit != "miss") {
+			// reset direction/speed here
+			if ((sideHit === "left") || (sideHit === "right")) {
+				myGameBall.speedX = myGameBall.speedX * -1;
+			}
+			
+			if ((sideHit === "top") || (sideHit === "bottom")) {
+				myGameBall.speedY = myGameBall.speedY * -1;
+			}
+			myScoreValue += myObstacles[i].scoreValue;
+		}
+	}
+	myGameBall.newPos();
+	
+	// Set paddle movement
+	myGamePiece.speedX = 0;
+	myGamePiece.speedY = 0;
+	if (myGameArea.x) {
+		if(myGameArea.x > WIDTH - myGamePiece.width) {
+			myGamePiece.x = WIDTH - myGamePiece.width;
+		} else {
+			myGamePiece.x = myGameArea.x;
+		}
+	}
+	if (myGameArea.key && (myGameArea.key === 37 || myGameArea.key === 65)) {	//left (arrow or "a")
+		myGamePiece.speedX = -1; 
+	}
+	if (myGameArea.key && (myGameArea.key === 39 || myGameArea.key === 68)) {	//right (arrow or "d")
+		myGamePiece.speedX = 1; 
+	}
+	myGamePiece.newPos();
+	
+	// Show the Score
+	myScore.text = "SCORE: " + myScoreValue;
+	
+    myScore.update();
+	myGameBall.update();
+	myGamePiece.update();
+	
+	if (SHOW_TEST_DATA) {
+		showTestData_GamePiece.text = "x: " + myGamePiece.x + "\ty: " + myGamePiece.y + "\n" +
+							"speedX:" + myGamePiece.speedX + "\tspeedY: " + myGamePiece.speedY;
+		showTestData_GameBall.text = "x: " + myGameBall.x + "\ty: " + myGameBall.y + "\n" +
+							"speedX:" + myGameBall.speedX + "\tspeedY: " + myGameBall.speedY;
+		showTestData_GamePiece.update();
+		showTestData_GameBall.update();
+	}
+}
+
