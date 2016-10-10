29db03ad7691051dcbe9d3839369c77361672a1b
Skills Game <dd.skills.game@gmail.com>
2016-10-10 17:33:18 -0600

    change password

--- TaskManager/includes/db.php
+++ TaskManager/includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_HOST = '73.229.5.101';
 $DB_USER = 'taskuser';
-$DB_PASS = 'wofM2DlztnlSrZF3';
+$DB_PASS = 'tcfZbm7TLnTE7a17';
 $DB_NAME = 'Tasks';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
